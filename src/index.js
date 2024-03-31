407b516e874218a02a2f24926b8f0f775a6be86e
rupesh00mahat <rupesh00mahat@gmail.com>
2024-03-31 19:48:40 +0545

    fix error

--- src/index.js
+++ src/index.js
@@ -26,11 +26,9 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-      <HashRouter>
       <Provider store={store}>
     <RouterProvider router={router} />
     
     </Provider>
-      </HashRouter>
   </React.StrictMode>
 );
